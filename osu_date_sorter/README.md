e76e934bd197d5899b2b0e09e72db603abe25e92
molneya <>
2024-07-02 00:38:09 +0800

    readme and usage improvements

---
+++ osu_date_sorter/README.md
@@ -0,0 +1,22 @@
+# osu! date sorter
+
+Updates your osu!.db and Songs directory to allow sorting by Ranked Date using the Date Added sort in game.
+
+## Usage
+
+To download and run the help command:
+
+```bash
+git clone https://github.com/molneya/osu_date_sorter
+cd osu_date_sorter
+python osu_date_sorter --help
+```
+
+To make the program update your stuff, you need to supply your osu! directory, for example:
+
+```bash
+python osu_date_sorter E:\osu!
+```
+
+Make sure you close osu! before running this command or else osu! will overwrite it.
+I would also recommend backing up your osu!.db before doing this. I am not responsible for any database corruption or data loss you may experience.
--- osu_date_sorter/__main__.py
+++ osu_date_sorter/__main__.py
@@ -1,14 +1,14 @@
 
 import argparse
 import gzip
+import os
 import pathlib
 import time
 from sorter import load_data, update_db_last_modified, update_file_last_modified
 
 def main():
     parser = argparse.ArgumentParser(prog="osu_date_sorter", description="Overwrites last_modified in osu!.db to allow sorting by date ranked")
-    parser.add_argument("osu_db", help="osu!.db file", type=argparse.FileType("rb+"))
-    parser.add_argument("osu_songs", help="osu! Songs folder", type=pathlib.Path)
+    parser.add_argument("osu_dir", help="your base osu! directory", type=pathlib.Path)
     args = parser.parse_args()
 
     print("If you haven't made backups, press CTRL+C to terminate the program. Also, make sure osu! is closed. I do not claim responsibility for any corruptions that may occur.")
@@ -18,8 +18,12 @@ def main():
     with gzip.open("ranked_data.gz", 'rb') as f:
         ranked_data = load_data(f)
 
-    update_db_last_modified(args.osu_db, ranked_data)
-    update_file_last_modified(args.osu_songs, ranked_data)
+    osu_db_path = os.path.join(args.osu_dir, "osu!.db")
+    with open(osu_db_path, 'rb+') as f:
+        update_db_last_modified(f, ranked_data)
+
+    osu_song_dir = os.path.join(args.osu_dir, "Songs")
+    update_file_last_modified(osu_song_dir, ranked_data)
 
 if __name__ == "__main__":
     exit(main())
